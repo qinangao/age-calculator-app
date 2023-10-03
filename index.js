799d0af0bdf744b87028687d28b2d48ac12756df
Qinan Gao <qinanagao@gmail.com>
2023-10-03 22:44:36 +0100

    add calculation function

--- index.js
+++ index.js
@@ -1,38 +1,64 @@
-let birthday = document.querySelectorAll(".birthday");
-let errorMessages = document.querySelectorAll(".error_msg");
-let inputBorders = document.querySelectorAll(".input_box");
-let dayValue = document.getElementById("days_id").value;
-let monthValue = document.getElementById("month_id").value;
-let yearValue = document.getElementById("year_id").value;
+// Part 1- Calculate Age:
 
-function checker() {
-  if (dayValue === "" && monthValue === "" && yearValue === "") {
-    birthday.forEach((item) => {
-      item.style.color = "var(--Light-red)";
-    });
-    inputBorders.forEach((item) => {
-      item.style.border = "3px solid var(--Light-red)";
-    });
-    errorMessages.forEach((item) => {
-      item.style.display = "block";
-    });
-  } else if (
-    dayValue < 0 &&
-    dayValue > 31 &&
-    monthValue < 0 &&
-    monthValue > 12 &&
-    yearValue < 0 &&
-    yearValue > 2023
-  ) {
-    birthday.forEach((item) => {
-      item.style.color = "var(--Light-red)";
-    });
-    inputBorders.forEach((item) => {
-      item.style.border = "3px solid var(--Light-red)";
-    });
-    errorMessages.forEach((item) => {
-      item.style.display = "block";
-      item.style.color = "black";
-    });
+function calculateAge() {
+  let dayValue = parseInt(document.getElementById("days_id").value);
+  let monthValue = parseInt(document.getElementById("month_id").value);
+  let yearValue = parseInt(document.getElementById("year_id").value);
+  let dayResult = document.getElementById("day_result");
+  let monthResult = document.getElementById("month_result");
+  let yearResult = document.getElementById("year_result");
+
+  const today = new Date();
+  const birthDate = new Date(yearValue, monthValue - 1, dayValue); // month is 0-based
+
+  let years = today.getFullYear() - birthDate.getFullYear();
+  let months = today.getMonth() - birthDate.getMonth();
+  let days = today.getDate() - birthDate.getDate();
+
+  // If the birth date has not occurred this year yet
+  if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate())) {
+    years--;
+    months += 12;
   }
+
+  // Adjust days for negative values
+  if (days < 0) {
+    months--;
+    days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+  }
+
+  yearResult.innerHTML = years;
+  monthResult.innerHTML = months;
+  dayResult.innerHTML = days;
 }
+
+// let inputBorders = document.querySelectorAll(".input_box");
+// let birthday = document.querySelectorAll(".birthday");
+// let errorMessages = document.querySelectorAll(".error_msg");
+
+// let dateError = document.getElementById("date_error");
+// let monthError = document.getElementById("month_error");
+// let yearError = document.getElementById("year_error");
+
+// function checker() {
+//   if (dayValue === "" && monthValue === "" && yearValue === "") {
+//     birthday.forEach((item) => {
+//       item.style.color = "var(--Light-red)";
+//     });
+//     inputBorders.forEach((item) => {
+//       item.style.border = "3px solid var(--Light-red)";
+//     });
+//     errorMessages.forEach((item) => {
+//       item.style.display = "block";
+//     });
+//   }
+//   if (dayValue < 1 || dayValue > 31) {
+//     dateError.innerHTML = "Must be a valid day";
+//   }
+//   if (monthValue < 1 || monthValue > 13) {
+//     monthError.innerHTML = "Must be a valid month";
+//   }
+//   if (monthValue > currentYear) {
+//     yearError.innerHTML = "Must be in the past";
+//   }
+// }
