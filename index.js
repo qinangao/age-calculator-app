7f6c7a9875eccc1b632fd41fdd61946bbc8e8923
Qinan Gao <qinanagao@gmail.com>
2023-10-04 22:51:12 +0100

    add checker

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Part 1- Calculate Age:
+// Part 1- Check valid:
 
 function calculateAge() {
   let dayValue = parseInt(document.getElementById("days_id").value);
@@ -8,8 +8,61 @@ function calculateAge() {
   let monthResult = document.getElementById("month_result");
   let yearResult = document.getElementById("year_result");
 
+  let dateError = document.getElementById("date_error");
+  let monthError = document.getElementById("month_error");
+  let yearError = document.getElementById("year_error");
+
+  let inputBorders = document.querySelectorAll(".input_box");
+  let errorMessages = document.querySelectorAll(".error_msg");
+
+  // Check if the year input is a valid number
+  if (yearValue > 2023 || yearValue <= 0) {
+    yearError.innerHTML = "Must be in the past";
+    yearResult.innerHTML = "--";
+    errorMessages.forEach((item) => {
+      item.style.display = "block";
+    });
+    inputBorders.forEach((item) => {
+      item.style.border = "3px solid var(--Light-red)";
+    });
+    return;
+  }
+
+  // Check if the month input is valid (between 1 and 12)
+  if (monthValue < 1 || monthValue > 12) {
+    monthError.innerHTML = "Must be a valid month";
+    yearResult.innerHTML = "--";
+    errorMessages.forEach((item) => {
+      item.style.display = "block";
+    });
+    inputBorders.forEach((item) => {
+      item.style.border = "3px solid var(--Light-red)";
+    });
+    return;
+  }
+
+  // Check if the day input is valid for the selected month
+  const maxDaysInMonth = new Date(yearValue, monthValue, 0).getDate();
+  if (dayValue < 1 || dayValue > maxDaysInMonth) {
+    dateError.innerHTML = "Must be a valid date";
+    yearResult.innerHTML = "--";
+    errorMessages.forEach((item) => {
+      item.style.display = "block";
+    });
+    inputBorders.forEach((item) => {
+      item.style.border = "3px solid var(--Light-red)";
+    });
+    return;
+  }
+
   const today = new Date();
   const birthDate = new Date(yearValue, monthValue - 1, dayValue); // month is 0-based
+
+  // Check if the input year is in the past
+  if (birthDate > today) {
+    dateError.innerHTML = "Must be a valid date";
+    yearResult.innerHTML = "--";
+  }
 
   let years = today.getFullYear() - birthDate.getFullYear();
   let months = today.getMonth() - birthDate.getMonth();
@@ -31,34 +84,3 @@ function calculateAge() {
   monthResult.innerHTML = months;
   dayResult.innerHTML = days;
 }
-
-// let inputBorders = document.querySelectorAll(".input_box");
-// let birthday = document.querySelectorAll(".birthday");
-// let errorMessages = document.querySelectorAll(".error_msg");
-
-// let dateError = document.getElementById("date_error");
-// let monthError = document.getElementById("month_error");
-// let yearError = document.getElementById("year_error");
-
-// function checker() {
-//   if (dayValue === "" && monthValue === "" && yearValue === "") {
-//     birthday.forEach((item) => {
-//       item.style.color = "var(--Light-red)";
-//     });
-//     inputBorders.forEach((item) => {
-//       item.style.border = "3px solid var(--Light-red)";
-//     });
-//     errorMessages.forEach((item) => {
-//       item.style.display = "block";
-//     });
-//   }
-//   if (dayValue < 1 || dayValue > 31) {
-//     dateError.innerHTML = "Must be a valid day";
-//   }
-//   if (monthValue < 1 || monthValue > 13) {
-//     monthError.innerHTML = "Must be a valid month";
-//   }
-//   if (monthValue > currentYear) {
-//     yearError.innerHTML = "Must be in the past";
-//   }
-// }
